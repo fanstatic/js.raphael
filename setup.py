cedc73cc34fff1bfde2e0c5fe72571c46afe494f
Andreas Kaiser <disko@binary-punks.com>
2013-03-06 17:41:07 +0100

    Fix package metadata.  Remove unneeded dependencies.

--- setup.py
+++ setup.py
@@ -3,18 +3,15 @@ import os
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.1.0'
+version = '2.1.0-1'
 
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-long_description = (
-    read('README.txt')
-    + '\n' +
-    read('js', 'raphael', 'test_raphael.txt')
-    + '\n' +
-    read('CHANGES.txt'))
+long_description = (read('README.txt') + '\n' +
+                    read('js', 'raphael', 'test_raphael.txt') + '\n' +
+                    read('CHANGES.txt'))
 
 setup(
     name='js.raphael',
@@ -23,21 +20,21 @@ setup(
     long_description=long_description,
     classifiers=[],
     keywords='',
-    author='Fanstatic Developers',
-    author_email='fanstatic@googlegroups.com',
+    author='Andreas Kaiser',
+    author_email='disko@binary-punks.com',
+    url='https://github.com/disko/js.raphael',
     license='BSD',
     packages=find_packages(),
     namespace_packages=['js'],
     include_package_data=True,
     zip_safe=False,
-    setup_requires=['hgtools'],
     install_requires=[
         'fanstatic',
         'setuptools',
-        ],
+    ],
     entry_points={
         'fanstatic.libraries': [
             'raphael = js.raphael:library',
-            ],
-        },
-    )
+        ],
+    },
+)
